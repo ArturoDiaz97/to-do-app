bc9ebc40192d193875a92a8bcbe77bebf9eceaae
ArturoDiaz97 <arturoallca@gmail.com>
2021-08-22 22:33:18 -0500

    navegacion de tabs añadido

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 ((d)=>{
-    console.log("Funciona");
+    const tabs = d.querySelectorAll(".tab");
+    const listas = d.querySelectorAll(".list-container");
+    tabs.forEach((tab, index) => {
+        tab.addEventListener("click", () => {
+            tabs.forEach((tab, index) => {
+                tab.classList.remove("activo");
+                listas[index].classList.remove("activo");
+            });
+            tab.classList.add("activo");
+            listas[index].classList.add("activo");
+        })
+    });
 })(document);
