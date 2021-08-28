62b8206f89af2107ff717369c22204e38d20ffff
ArturoDiaz97 <arturoallca@gmail.com>
2021-08-28 12:33:26 -0500

    creador de template desacoplado

---
+++ js/create-template-todo.js
@@ -0,0 +1,58 @@
+export function createTemplateTODO(d, listaActivos, listaAll, itemFragment, target){
+    let $inputCheckboxContainer = d.createElement("div");
+    $inputCheckboxContainer.classList.add("input-checkbox-container");
+    $inputCheckboxContainer.classList.add("item")
+    
+    let $checkboxContainer = d.createElement("div");
+    $checkboxContainer.classList.add("checkbox-container");
+    
+    let $checkBoxCircle = d.createElement("label");
+    $checkBoxCircle.classList.add("checkbox-circle");
+    
+    let $input = d.createElement("input");
+    $input.classList.add("checkbox-item");
+    $input.type = "checkbox";
+    
+    let $checkboxText = d.createElement("span");
+    $checkboxText.classList.add("checkbox-text");
+    let $checkboxCircleCenter = d.createElement("div");
+    $checkboxCircleCenter.classList.add("checkbox-circle-center");
+    
+    $checkBoxCircle.appendChild($input);
+    $checkBoxCircle.appendChild($checkboxText);
+    $checkBoxCircle.appendChild($checkboxCircleCenter);
+    
+    $checkboxContainer.appendChild($checkBoxCircle); // se armo el checkbox
+    
+    let $inputContainer = d.createElement("div");
+    $inputContainer.classList.add("input-container");
+    
+    let $textIconContainer = d.createElement("div");
+    $textIconContainer.classList.add("text-icon-container");
+    
+    let $itemText = d.createElement("div");
+    $itemText.classList.add("item-text");
+    $itemText.textContent = target.value;
+    
+    let $icon = d.createElement("img");
+    $icon.classList.add("icon");
+    $icon.src = "assets/img/icon-cross.svg";
+    $icon.alt = "cross-icon";
+    
+    $textIconContainer.appendChild($itemText);
+    $textIconContainer.appendChild($icon);
+    
+    $inputContainer.appendChild($textIconContainer); // se armo el input
+    
+    $inputCheckboxContainer.appendChild($checkboxContainer);
+    $inputCheckboxContainer.appendChild($inputContainer);
+    
+    itemFragment.appendChild($inputCheckboxContainer);
+    let itemFramentClone = itemFragment.cloneNode(true);
+    
+    
+    listaActivos.appendChild(itemFragment);
+    listaAll.appendChild(itemFramentClone);
+    
+    target.value = "";
+}
